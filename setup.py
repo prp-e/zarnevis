bc2298d3940f1288d2e12db76c428444af859516
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-20 18:08:17 +0430

    Added some needed information

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ with open('README.md') as f:
 setup(
     name='zarnevis',
     version='0.0.1', 
+    author='Muhammadreza Haghiri',
+    author_email='haghiri75@gmail.com',
+    url='https://github.com/prp-e/zarnevis',
     description='Zarnevis, a tool for writing RTL text in computer vision projects',
     long_description_content_type='text/markdown',
     long_description=long_description,
+
 )
