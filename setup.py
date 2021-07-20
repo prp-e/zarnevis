5f66b160b36a536f842cfee7a260501a18f9f106
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-20 18:27:53 +0430

    Working almost done, let's test!

--- setup.py
+++ setup.py
@@ -13,4 +13,12 @@ setup(
     long_description_content_type='text/markdown',
     long_description=long_description,
     packages=find_packages(),
+    install_requires=['numpy', 'pillow', 'arabic-reshaper', 'python-bidi'],
+    keywords = ['persian', 'arabic', 'rtl', 'text', 'cv', 'computer-vision'],
+    classifiers = ["Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",]
 )
